06caab79c523268204593f30261306cd5ca13e65
Rafael Torres <rdtorres@gmail.com>
2013-11-12 12:20:17 -0500

    Preventing parallel execution

--- globoDownloader.sh
+++ globoDownloader.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-cd $(dirname $0)
+#configure the env variable below based on how your system is configured
 
+PYTHON_EXEC=/opt/Python-2.7/bin/python
+export PYTHONPATH=/home/rafael/globo.tv/plugin.video.globotv-plugin.video.globotv-0.5.5.1/
+
+
+cd $(dirname $0)
 #Store start time in a variable to calculate total execution time
-START=$(/opt/Python-2.7/bin/python -c "from time import *; print int(time())")
+START=$($PYTHON_EXEC -c "from time import *; print int(time())")
 
 echo "******************************************** "
 echo "**    run on $(date) "
 echo "******************************************** "
 
 
-PYTHONPATH=/home/rafael/globo.tv/plugin.video.globotv-plugin.video.globotv-0.5.5.1/ /opt/Python-2.7/bin/python globoDownloader.py $@
+running=$(ps -eaf | grep globoDownloader.py | grep -v grep | wc -l)
+echo "** running job $(basename $0) on $(date) running = $running"
 
-END=$(/opt/Python-2.7/bin/python -c "from time import *; print int(time())")
+if [ $running -gt 0 ]; then
+    echo "** job already running @ running = $running ..."
+    echo "** will not run again... "
+else
+    $PYTHON_EXEC ./globoDownloader.py $@
+fi
+
+
+
+END=$($PYTHON_EXEC -c "from time import *; print int(time())")
 
 #Calculates difference
 DIFF=$(( $END - $START ))

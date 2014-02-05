68466017e1d6dc9bf4c416f00b0d8a535d075fc7
Rafael Torres <rdtorres@gmail.com>
2014-02-05 11:47:14 -0500

    Set log level to debug

--- FakePlugin.py
+++ FakePlugin.py
@@ -1,19 +1,5 @@
 '''
      FakePlugin override.
-
-     This program is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation, either version 3 of the License, or
-     (at your option) any later version.
-     
-     This program is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
-     
-     You should have received a copy of the GNU General Public License
-     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 '''
 import logging
 import ConfigParser
@@ -27,9 +13,10 @@ class FakePlugin:
         #logging.config.fileConfig(iniFile)
         self.tainted = False
         self.log = logging.getLogger('FakePlugin')
+        logging.basicConfig(level=logging.DEBUG)
         self.props = ConfigParser.RawConfigParser()
         if os.path.exists(self.propFile):
-            self.log.info('Using Properties file:',  self.propFile)
+            self.log.info('Using Properties file: %s',  self.propFile)
             self.props.read(self.propFile)
         else:
             print datetime.now(), 'properties file ' +  self.propFile + ' does not exist in script directory'
